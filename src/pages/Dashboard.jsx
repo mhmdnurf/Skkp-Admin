05b11fa06b2b2b9432a4f3a9e9de5b79064396a6
mhmdnurf <zaldebarenz@gmail.com>
2023-08-12 14:11:18 +0700

    first commit

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+const Dashboard = () => {
+  return (
+    <div className="dashboard-content">
+      <h1>Dashboard Content</h1>
+      <p>This is the main content of the dashboard.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
